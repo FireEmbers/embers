837b39899354c12bc133cbc132bf3eac739761d5
pedrombafonso <mail@pedroafonso.net>
2014-01-23 18:25:05 +0000

    post-processing

---
+++ post-processing.js
@@ -0,0 +1,37 @@
+var StatsArray = require('./stats-array/stats-array.js');
+
+var Embers = {
+  maps  : [],
+  avg   : [],
+  ldev  : [],
+  udev  : [],
+
+  addMap:  function (map) {
+    this.maps.push(map);
+  },
+
+  process: function() {
+    var values = [];
+    for (var pos = 0; pos < this.maps[0].length; pos++) {
+      var array = [];
+      for (var i = 0; i < this.maps.length; i++) {
+        var map = this.maps[i];
+        array.push(map[pos]);
+      }
+      var mean = array.mean();
+      var moe = array.marginOfError(0.90);
+      this.avg.push(mean);
+      this.ldev.push(mean - moe);
+      this.udev.push(mean + moe);
+    }
+
+    return {
+      avg: this.avg,
+      ldev: this.ldev,
+      udev: this.udev
+    };
+  }
+
+};
+
+module.exports = Embers;
---
+++ test-post-processing.js
@@ -0,0 +1,16 @@
+var embers = require('./index.js');
+
+var maps =[
+    [1, 3, 20],
+    [2, 2, 30],
+    [3, 1, 40],
+    [4, 4, 50]
+];
+
+for(var i = 0; i < maps.length; i++) {
+  embers.addMap(maps[i]);
+}
+
+console.log(embers);
+var finalMaps = embers.process();
+console.log(finalMaps);
